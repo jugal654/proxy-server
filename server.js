3f8ac4eff4dc6d1cb0e0368662e17d088fffee20
jugal654 <jugalpurohit704@gmail.com>
2026-03-29 11:19:35 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const { createProxyServer } = require('http-proxy');
 
 const proxy = createProxyServer({
   target: 'http://slkbullion.com:10001',
-  ws: true
+  ws: true,
+  changeOrigin: true
+});
+
+proxy.on('error', (err, req, res) => {
+  console.error('Proxy error:', err);
 });
 
 const server = http.createServer((req, res) => {
   proxy.web(req, res);
 });
 
+// 🔥 VERY IMPORTANT (WebSocket fix)
 server.on('upgrade', (req, socket, head) => {
   proxy.ws(req, socket, head);
 });
